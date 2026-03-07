358276731b0a86301c4ae8df047c330ab9e8cc0d
strawgate <strawgate@users.noreply.github.com>
2026-03-07 18:31:33 +0000

    Deploy preview for PR 1938 🛫

--- pr-preview/pr-1938/assets/useSearchParam-DnWDh-za.js
+++
@@ -1,2 +0,0 @@
-import{d as r,c as e}from"./index-CIwy3KcN.js";function a(){return r("search",e.withDefault("").withOptions({history:"replace"}))}export{a as u};
-//# sourceMappingURL=useSearchParam-DnWDh-za.js.map
