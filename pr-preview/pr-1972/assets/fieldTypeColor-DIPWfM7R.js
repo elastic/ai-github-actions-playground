25e45d9d4703d715535a750036f3f20e92fde31e
strawgate <strawgate@users.noreply.github.com>
2026-03-07 19:35:04 +0000

    Deploy preview for PR 1972 🛫

---
+++ pr-preview/pr-1972/assets/fieldTypeColor-DIPWfM7R.js
@@ -0,0 +1,2 @@
+function n(r){return r==="date"||r==="date_nanos"?"warning":r==="long"||r==="integer"||r==="double"||r==="float"||r==="short"||r==="byte"?"primary":r==="boolean"?"secondary":r==="keyword"||r==="text"||r==="ip"||r==="version"?"success":"default"}export{n as g};
+//# sourceMappingURL=fieldTypeColor-DIPWfM7R.js.map
---
+++ pr-preview/pr-1972/assets/useSearchParam-CkL78HlL.js
@@ -0,0 +1,2 @@
+import{d as r,c as e}from"./index-DhsCDQLu.js";function a(){return r("search",e.withDefault("").withOptions({history:"replace"}))}export{a as u};
+//# sourceMappingURL=useSearchParam-CkL78HlL.js.map
