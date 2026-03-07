18805ed9ce72c5cd455c056491e66924288abbba
strawgate <strawgate@users.noreply.github.com>
2026-03-07 18:28:06 +0000

    Deploy preview for PR 1967 🛫

---
+++ pr-preview/pr-1967/assets/useSearchParam-Ba_d3SAi.js
@@ -0,0 +1,2 @@
+import{d as r,c as e}from"./index-7TVpulAI.js";function a(){return r("search",e.withDefault("").withOptions({history:"replace"}))}export{a as u};
+//# sourceMappingURL=useSearchParam-Ba_d3SAi.js.map
