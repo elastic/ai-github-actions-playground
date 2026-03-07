4dce52e6ea32838a566cb90de6558fb06044df1e
strawgate <strawgate@users.noreply.github.com>
2026-03-07 21:13:00 +0000

    Deploy preview for PR 1974 🛫

--- pr-preview/pr-1974/assets/useTableSort-BZuuXJbp.js
+++
@@ -1,2 +0,0 @@
-import{r}from"./mui-Cgz-MpXb.js";function k(S,o="asc"){const[s,e]=r.useState(S),[c,n]=r.useState(o),a=r.useCallback(t=>{e(t),n(p=>s===t?p==="asc"?"desc":"asc":o)},[s,o]),b=r.useCallback(t=>({active:s===t,direction:s===t?c:o,onClick:()=>a(t)}),[s,c,o,a]);return{sortField:s,sortDirection:c,handleSort:a,getSortLabelProps:b,setSortField:e,setSortDirection:n}}export{k as u};
-//# sourceMappingURL=useTableSort-BZuuXJbp.js.map
