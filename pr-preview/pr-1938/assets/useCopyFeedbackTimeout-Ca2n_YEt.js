1b6e443ca3fb0436663588ff8c8ba186b0fea7d4
strawgate <strawgate@users.noreply.github.com>
2026-03-07 20:00:45 +0000

    Deploy preview for PR 1938 🛫

--- pr-preview/pr-1938/assets/useCopyFeedbackTimeout-Ca2n_YEt.js
+++
@@ -1,2 +0,0 @@
-import{r}from"./mui-CaLbWdrX.js";function n(t,u=2e3){const e=r.useRef(null);return r.useEffect(()=>()=>{e.current&&(clearTimeout(e.current),e.current=null)},[]),r.useCallback(()=>{e.current&&clearTimeout(e.current),e.current=setTimeout(t,u)},[u,t])}export{n as u};
-//# sourceMappingURL=useCopyFeedbackTimeout-Ca2n_YEt.js.map
