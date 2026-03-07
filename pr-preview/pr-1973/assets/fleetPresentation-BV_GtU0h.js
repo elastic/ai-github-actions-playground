fc2ef34a873fd8262376d9cbbe3c7b65b1936416
strawgate <strawgate@users.noreply.github.com>
2026-03-07 21:10:00 +0000

    Deploy preview for PR 1973 🛫

--- pr-preview/pr-1973/assets/fleetPresentation-BV_GtU0h.js
+++
@@ -1,2 +0,0 @@
-import{g as e,aC as t}from"./index-BFBSU_QP.js";function n(r){return r==="fresh"?"success":r==="stale"?"warning":"error"}function f(r){return r?t(r):""}function o(r){return r?e(r):""}export{o as a,f,n as s};
-//# sourceMappingURL=fleetPresentation-BV_GtU0h.js.map
---
+++ pr-preview/pr-1973/assets/useSearchParam-CqJoo3My.js
@@ -0,0 +1,2 @@
+import{d as r,c as e}from"./index-DLxo3SqN.js";function a(){return r("search",e.withDefault("").withOptions({history:"replace"}))}export{a as u};
+//# sourceMappingURL=useSearchParam-CqJoo3My.js.map
