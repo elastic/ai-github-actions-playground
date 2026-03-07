303d8364b88267ff4a2c9258fe193572067f0c37
strawgate <strawgate@users.noreply.github.com>
2026-03-07 23:33:11 +0000

    Remove preview for PR 1983 🛬

--- pr-preview/pr-1983/assets/useSearchParam-BgIgNZNs.js
+++
@@ -1,2 +0,0 @@
-import{d as r,c as e}from"./index-BShy23di.js";function a(){return r("search",e.withDefault("").withOptions({history:"replace"}))}export{a as u};
-//# sourceMappingURL=useSearchParam-BgIgNZNs.js.map
