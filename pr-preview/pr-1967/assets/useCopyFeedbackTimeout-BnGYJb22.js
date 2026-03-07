b596d891af22303ae052f2f9e7a745e398ab71fa
strawgate <strawgate@users.noreply.github.com>
2026-03-07 19:44:35 +0000

    Remove preview for PR 1967 🛬

--- pr-preview/pr-1967/assets/useCopyFeedbackTimeout-BnGYJb22.js
+++
@@ -1,2 +0,0 @@
-import{r}from"./mui-Cgz-MpXb.js";function n(t,u=2e3){const e=r.useRef(null);return r.useEffect(()=>()=>{e.current&&(clearTimeout(e.current),e.current=null)},[]),r.useCallback(()=>{e.current&&clearTimeout(e.current),e.current=setTimeout(t,u)},[u,t])}export{n as u};
-//# sourceMappingURL=useCopyFeedbackTimeout-BnGYJb22.js.map
