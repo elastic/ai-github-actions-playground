56aafdd6b1a285676626929ee3b2ea2212183e84
strawgate <strawgate@users.noreply.github.com>
2026-03-07 20:32:14 +0000

    Deploy preview for PR 1976 🛫

---
+++ pr-preview/pr-1976/assets/useSearchParam-BxR6bihH.js
@@ -0,0 +1,2 @@
+import{d as r,c as e}from"./index-CdqLpO_t.js";function a(){return r("search",e.withDefault("").withOptions({history:"replace"}))}export{a as u};
+//# sourceMappingURL=useSearchParam-BxR6bihH.js.map
