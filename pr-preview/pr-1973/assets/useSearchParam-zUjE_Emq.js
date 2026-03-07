9e1e63a95ede2a196291cfb01f1637cc424a0755
strawgate <strawgate@users.noreply.github.com>
2026-03-07 20:55:34 +0000

    Deploy preview for PR 1973 🛫

---
+++ pr-preview/pr-1973/assets/useSearchParam-zUjE_Emq.js
@@ -0,0 +1,2 @@
+import{d as r,c as e}from"./index-DqgI6DaJ.js";function a(){return r("search",e.withDefault("").withOptions({history:"replace"}))}export{a as u};
+//# sourceMappingURL=useSearchParam-zUjE_Emq.js.map
