8f94ee83b7085d37239866934da39dc7a1209330
strawgate <strawgate@users.noreply.github.com>
2026-03-07 19:09:35 +0000

    Deploy preview for PR 1971 🛫

---
+++ pr-preview/pr-1971/assets/useSearchParam-CiP0-oig.js
@@ -0,0 +1,2 @@
+import{d as r,c as e}from"./index-Bpc7aBO7.js";function a(){return r("search",e.withDefault("").withOptions({history:"replace"}))}export{a as u};
+//# sourceMappingURL=useSearchParam-CiP0-oig.js.map
