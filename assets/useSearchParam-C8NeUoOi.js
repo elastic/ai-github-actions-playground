654234afe0338aef918d20bf6ff740ac9001f49f
strawgate <williamseaston@gmail.com>
2026-03-07 22:08:37 +0000

    Deploying to gh-pages from @ elastic/ai-github-actions-playground@8de3e02b3f24833aa24cc22b36ba23106d42886d 🚀

--- assets/useSearchParam-C8NeUoOi.js
+++
@@ -1,2 +0,0 @@
-import{d as r,c as e}from"./index-COGTCtHh.js";function a(){return r("search",e.withDefault("").withOptions({history:"replace"}))}export{a as u};
-//# sourceMappingURL=useSearchParam-C8NeUoOi.js.map
