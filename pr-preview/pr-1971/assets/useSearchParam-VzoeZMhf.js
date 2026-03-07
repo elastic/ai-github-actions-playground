1b277d3842e3665ba224aef0f8204f012d9ae0dd
strawgate <strawgate@users.noreply.github.com>
2026-03-07 20:16:00 +0000

    Deploy preview for PR 1971 🛫

---
+++ pr-preview/pr-1971/assets/useSearchParam-VzoeZMhf.js
@@ -0,0 +1,2 @@
+import{d as r,c as e}from"./index-jP4cQs02.js";function a(){return r("search",e.withDefault("").withOptions({history:"replace"}))}export{a as u};
+//# sourceMappingURL=useSearchParam-VzoeZMhf.js.map
