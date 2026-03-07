f848853f6543a51e351be94eedd859369b8a66c2
strawgate <strawgate@users.noreply.github.com>
2026-03-07 18:25:49 +0000

    Deploy preview for PR 1967 🛫

---
+++ pr-preview/pr-1967/assets/useSearchParam-D1xicpFx.js
@@ -0,0 +1,2 @@
+import{d as r,c as e}from"./index-BXsfCJ7V.js";function a(){return r("search",e.withDefault("").withOptions({history:"replace"}))}export{a as u};
+//# sourceMappingURL=useSearchParam-D1xicpFx.js.map
