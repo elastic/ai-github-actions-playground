f9500e04e1653c83793ebadcc79dd058188ff76b
strawgate <strawgate@users.noreply.github.com>
2026-03-07 21:42:45 +0000

    Deploy preview for PR 1974 🛫

---
+++ pr-preview/pr-1974/assets/useSearchParam-B9hdOdQs.js
@@ -0,0 +1,2 @@
+import{d as r,c as e}from"./index-To7ArYTA.js";function a(){return r("search",e.withDefault("").withOptions({history:"replace"}))}export{a as u};
+//# sourceMappingURL=useSearchParam-B9hdOdQs.js.map
