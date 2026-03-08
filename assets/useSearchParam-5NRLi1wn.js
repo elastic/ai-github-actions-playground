1def50e1928674461ade0f354c740b077ecec20e
strawgate <williamseaston@gmail.com>
2026-03-08 08:12:21 +0000

    Deploying to gh-pages from @ elastic/ai-github-actions-playground@b59f6810d0398e55616c835bc12529412bf4fdd2 🚀

--- assets/useSearchParam-5NRLi1wn.js
+++
@@ -1,2 +0,0 @@
-import{d as r,c as e}from"./index-DMriC-W7.js";function a(){return r("search",e.withDefault("").withOptions({history:"replace"}))}export{a as u};
-//# sourceMappingURL=useSearchParam-5NRLi1wn.js.map
