acbac625c8195cda0f3f70566bcfa9cafca3f902
strawgate <strawgate@users.noreply.github.com>
2026-03-08 04:11:37 +0000

    Deploy preview for PR 2025 🛫

---
+++ pr-preview/pr-2025/assets/useSearchParam-kV7m_1_u.js
@@ -0,0 +1,2 @@
+import{d as r,c as e}from"./index-Cd8EwRmY.js";function a(){return r("search",e.withDefault("").withOptions({history:"replace"}))}export{a as u};
+//# sourceMappingURL=useSearchParam-kV7m_1_u.js.map
