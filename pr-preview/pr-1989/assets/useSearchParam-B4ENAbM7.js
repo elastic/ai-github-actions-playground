785d97f4ae484df9c1ee0a5ed92d3a8b8317c504
strawgate <strawgate@users.noreply.github.com>
2026-03-08 00:24:32 +0000

    Deploy preview for PR 1989 🛫

---
+++ pr-preview/pr-1989/assets/useSearchParam-B4ENAbM7.js
@@ -0,0 +1,2 @@
+import{d as r,c as e}from"./index-yxPju7w3.js";function a(){return r("search",e.withDefault("").withOptions({history:"replace"}))}export{a as u};
+//# sourceMappingURL=useSearchParam-B4ENAbM7.js.map
