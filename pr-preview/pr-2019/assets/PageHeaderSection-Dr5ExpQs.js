402e1048252801c6c2f56da733aff860dbd60094
strawgate <strawgate@users.noreply.github.com>
2026-03-08 06:44:24 +0000

    Deploy preview for PR 2019 🛫

--- pr-preview/pr-2019/assets/PageHeaderSection-Dr5ExpQs.js
+++
@@ -1,2 +0,0 @@
-import{j as e,aZ as a}from"./mui-BnWFVR9J.js";import{P as t}from"./PageHeader-D6C-LsbX.js";function i(r){return e.jsx(a,{variant:"outlined",sx:{p:1.5},children:e.jsx(t,{...r})})}export{i as P};
-//# sourceMappingURL=PageHeaderSection-Dr5ExpQs.js.map
---
+++ pr-preview/pr-2019/assets/useSearchParam-DLOJYY54.js
@@ -0,0 +1,2 @@
+import{d as r,c as e}from"./index-CCRP68pq.js";function a(){return r("search",e.withDefault("").withOptions({history:"replace"}))}export{a as u};
+//# sourceMappingURL=useSearchParam-DLOJYY54.js.map
