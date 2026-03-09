a6d6c9204ccfec01c3c9469893d2e0715fb5eb6c
strawgate <strawgate@users.noreply.github.com>
2026-03-09 05:07:09 +0000

    Deploy preview for PR 2181 🛫

---
+++ pr-preview/pr-2181/assets/useSearchParam-fZ3goBR3.js
@@ -0,0 +1,2 @@
+import{d as r,c as e}from"./index-Cp9734UW.js";function a(){return r("search",e.withDefault("").withOptions({history:"replace"}))}export{a as u};
+//# sourceMappingURL=useSearchParam-fZ3goBR3.js.map
