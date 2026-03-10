256ac16a612ee58168e4b82d66be83180548cbd7
strawgate <strawgate@users.noreply.github.com>
2026-03-10 00:31:50 +0000

    Deploy preview for PR 2298 🛫

---
+++ pr-preview/pr-2298/assets/useSearchParam-Dz15wxAI.js
@@ -0,0 +1,2 @@
+import{d as r,c as e}from"./index-CEn2Kcro.js";function a(){return r("search",e.withDefault("").withOptions({history:"replace"}))}export{a as u};
+//# sourceMappingURL=useSearchParam-Dz15wxAI.js.map
